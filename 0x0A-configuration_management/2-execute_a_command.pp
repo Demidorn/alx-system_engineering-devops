5c0dae2a19ca146ea7569e80b06f6679abde056e
Demidorn <dornkaizen@gmail.com>
2024-01-22 01:54:44 -0800

    kills a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # kills a proccesss named killmenow
 exec { 'killmenow':
-command     => '/usr/bin/pkill -f killmenow',
-refreshonly => true,
-logoutput   => true,
+command   => '/usr/bin/pkill -f killmenow',
+path      => '/usr/bin/',
 }
