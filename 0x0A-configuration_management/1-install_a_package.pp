c9fc5fea2d813814449d7e4fe2cc72cc5f642ece
Demidorn <dornkaizen@gmail.com>
2024-01-22 01:24:03 -0800

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,14 @@
 # installs flask package from pip3
-package { 'python3-pip':
-ensure   => 'installed',
+package { 'pip3':
+ensure   => '3..8.10',
 }
 
 package { 'flask':
 ensure   => '2.1.0',
 provider => 'pip3',
-require  => package['python3-pip'],
 }
 
 package { 'Werkzeug':
 ensure   => '2.1.1',
 provider => 'pip3',
-require  => package['python3-pip'],
 }
