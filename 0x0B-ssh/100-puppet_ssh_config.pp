2e90bf6c874065f84a1c761098178ede4718ec6d
Demidorn <dornkaizen@gmail.com>
2024-01-23 22:13:38 -0800

    puppet server configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 # client SSH configuration file using puppet
+
+file { '/home/ubuntu/.ssh':
+  ensure  => directory,
+  mode    => '0700',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
 file { '/home/ubuntu/.ssh/config':
-ensure  => present,
-mode	=> '0600',
-content => @(SSHCONFIG)
-Host *
-IdentifyFile ~/.ssh/school
-PasswordAuthentication no
-SSHCONFIG
-,
-owner	=> 'ubuntu',
-group	=> 'ubuntu',
+  ensure  => present,
+  mode    => '0600',
+  content => @(SSHCONFIG)
+    Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  SSHCONFIG
+  ,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
 }
