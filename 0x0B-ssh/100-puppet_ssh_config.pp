d2f7bd630732faea8838582a38794948c3ce5d89
Demidorn <dornkaizen@gmail.com>
2024-01-24 17:07:58 -0800

    puppet ssh configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,19 @@
 #!/usr/bin/env bash
 # client SSH configuration file using puppet
-file { '/home/ubuntu':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file { '/home/ubuntu/.ssh':
-  ensure  => directory,
-  mode    => '0700',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-}
-
-file { '/home/ubuntu/.ssh/config':
-  ensure  => present,
-  mode    => '0600',
-  content => @(SSHCONFIG)
-    Host *
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-  SSHCONFIG
-  ,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
