37298c92b19d83ee5a281bcc1f854048a9855693
Demidorn <dornkaizen@gmail.com>
2024-02-19 13:17:43 -0800

    API binding

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,22 +10,20 @@ import sys
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
     employee_id = sys.argv[1]
-    user_response = requests.get(f"{api_url}/users/{employee_id}")
-    req = user_response.json().get('name')
+    user_response = requests.get(f"{api_url}/users/{}".format(employee_id))
+    req = user_response.json().get('username')
     todos_response = requests.get(
             f"{api_url}/todos", params={"userId": employee_id})
     todos = todos_response.json()
+    user_data = {}
+    user_data[employee_id] = []
 
-    user_data = {
-        employee_id: [
-            {
-                "task": todo["title"],
-                "completed": todo["completed"],
-                "username": req
-            }
-            for todo in todos
-        ]
-    }
+    for tasks in todos:
+        t = {}
+        t['username'] = username
+        t['task'] = tasks.get('title')
+        t['completed'] = tasks.get('completed')
+        user_data[employee_id].append(t)
 
-    with open(f"{employee_id}.json", 'w') as json_file:
+    with open('{}.json'.format(employee_id), 'w') as json_file:
         json.dump(user_data, json_file)
