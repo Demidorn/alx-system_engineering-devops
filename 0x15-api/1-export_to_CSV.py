6147c3088299b18373fbf69930f647eec96e09e0
Demidorn <dornkaizen@gmail.com>
2024-02-19 12:14:21 -0800

    API binding

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,11 +3,9 @@
 Python script that exports data in the CSV format
 """
 
-
 import csv
 import requests
 import sys
-
 
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com"
@@ -15,21 +13,16 @@ if __name__ == "__main__":
     user_response = requests.get(f"{api_url}/users/{employee_id}")
     req = user_response.json().get('name')
     todos_response = requests.get(
-            f"{api_url}/todos", params={"userId": employee_id})
+        f"{api_url}/todos", params={"userId": employee_id})
     todos = todos_response.json()
-    todoss_complete = [task for task in todos if task["completed"]]
+    todos_complete = [task for task in todos if task["completed"]]
     total_task = len(todos_complete)
 
     with open('{}.csv'.format(employee_id), 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         csv_writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        todos_complete = [task for task in todos if task["completed"]]
-        completed_task = len(todos_complete)
-        """csvfile.write("Employee {} is done with tasks({}/{}):".format(
-            req, completed_task, total_task))"""
+            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
         for task in todos_complete:
             row = [employee_id, req, str(task["completed"]), task["title"]]
             csv_writer.writerow(row)
-            """csvfile.write("\t {}".format(task.get("title")))"""
