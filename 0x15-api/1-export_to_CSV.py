e68d59ee182467fd3b36ca9b172d261d17af1d89
Demidorn <dornkaizen@gmail.com>
2024-02-19 13:52:46 -0800

    API binding

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,8 +16,14 @@ if __name__ == "__main__":
         f"{api_url}/todos", params={"userId": employee_id})
     todos = todos_response.json()
 
-    with open('{}.csv'.format(employee_id), 'w', newline='') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+    with open('{}.csv'.format(employee_id), 'w') as csv_file:
+        for todos_response in todos:
+            completed = todos_response.get('completed')
+            title_task = todos_response.get('title')
+
+            csv_file.write('"{}","{}","{}","{}"\n'.format(
+                employee_id, req, completed, title_task))
+        """writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
         for task in todos:
             writer.writerow([employee_id, req, str(task.get('completed')),
-                            task.get('title')])
+                            task.get('title')])"""
